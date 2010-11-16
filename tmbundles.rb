6bb8a2a24bd0a26113b5ac646b763a0b4d0d6de2
Michael MacDonald <michaelm@amc.org.au>
2010-11-16 13:37:27 +1100

    Added initial tmbundles

---
+++ tmbundles.rb
@@ -0,0 +1,20 @@
+dep 'Ruby on Rails.tmbundle' do
+  source 'git://github.com/drnic/ruby-on-rails-tmbundle.git'
+end
+
+# TODO: what about setting the TM_CUCUMBER shell variable?
+dep 'Cucumber.tmbundle' do
+  source 'git://github.com/aslakhellesoy/cucumber-tmbundle.git'
+end
+
+dep 'Ruby Haml.tmbundle' do
+  source 'git://github.com/mattpolito/ruby-haml.tmbundle.git'
+end
+
+dep 'RSpec.tmbundle' do
+  source 'git://github.com/rspec/rspec-tmbundle.git'
+end
+
+dep 'SCSS.tmbundle' do
+  source 'git://github.com/kuroir/SCSS.tmbundle.git'
+end
